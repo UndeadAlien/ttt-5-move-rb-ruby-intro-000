e4dc7047fe3c89b00619a95365a2b4f2d028d2e7
Connor Hutchinson <workhutchc@gmail.com>
2020-11-07 04:12:57 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(index)
-  if index > 0 && index < 9
-    return index.to_i - 1
+  return index.to_i - 1
 end
 
 def move(array, index, value=" ")
